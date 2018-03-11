f3513346f22897dbf0e37fb96e990c8d719218a0
trapacska <trapacska@gmail.com>
2018-03-11 01:41:46 +0100

    .

---
+++ Multi Test Results Sample/Multi Test Results Sample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Multi Test Results Sample
+//
+//  Created by Tamás Papik on 3/10/18.
+//  Copyright © 2018 Tamas Papik. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Multi Test Results Sample/Multi Test Results SampleUITests/Multi_Test_Results_SampleUITests.swift
@@ -0,0 +1,153 @@
+//
+//  Multi_Test_Results_SampleUITests.swift
+//  Multi Test Results SampleUITests
+//
+//  Created by Tamás Papik on 3/10/18.
+//  Copyright © 2018 Tamas Papik. All rights reserved.
+//
+
+import XCTest
+
+class Multi_Test_Results_SampleUITests: XCTestCase {
+        
+    override func setUp() {
+        super.setUp()
+        
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+        
+        // In UI tests it is usually best to stop immediately when a failure occurs.
+        continueAfterFailure = false
+        // UI tests must launch the application that they test. Doing this in setup will make sure it happens for each test method.
+        XCUIApplication().launch()
+
+        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+   
+    func testCorrectTestExample0() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+        XCTAssert(4==2+2)
+    }
+    
+    func testCorrectTestExample1() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+        XCTAssert(4==2+2)
+    }
+    
+    func testCorrectTestExample2() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+        XCTAssert(4==2+2)
+    }
+    
+    func testCorrectTestExample3() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+        XCTAssert(4==2+2)
+    }
+    
+    func testCorrectTestExample4() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+        XCTAssert(4==2+2)
+    }
+    
+    func testCorrectTestExample5() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+        XCTAssert(4==2+2)
+    }
+    
+    func testCorrectTestExample6() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+        XCTAssert(4==2+2)
+    }
+    
+    func testCorrectTestExample7() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+        XCTAssert(4==2+2)
+    }
+    
+    func testCorrectTestExample8() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+        XCTAssert(4==2+2)
+    }
+    
+    func testCorrectTestExample9() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+        XCTAssert(4==2+2)
+    }
+    
+    func testFailedTestExample0() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+        XCTAssert(4==2+1)
+    }
+    
+    func testFailedTestExample1() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+        XCTAssert(4==2+1)
+    }
+    
+    func testFailedTestExample2() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+        XCTAssert(4==2+1)
+    }
+    
+    func testFailedTestExample3() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+        XCTAssert(4==2+1)
+    }
+    
+    func testFailedTestExample4() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+        XCTAssert(4==2+1)
+    }
+    
+    func testFailedTestExample5() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+        XCTAssert(4==2+1)
+    }
+    
+    func testFailedTestExample6() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+        XCTAssert(4==2+1)
+    }
+    
+    func testFailedTestExample7() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+        XCTAssert(4==2+1)
+    }
+    
+    func testFailedTestExample8() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+        XCTAssert(4==2+1)
+    }
+    
+    func testFailedTestExample9() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+        XCTAssert(4==2+1)
+    }
+    
+    
+}
